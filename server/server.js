81b389eb00924bd2c8ae962aad4e6089eff48674
robertm <minzatrobert@gmail.com>
2019-11-15 16:16:38 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3001;
+
+// var corsOptions = {
+// 	origin: 'http://localhost:3000',
+// 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+	console.log("received request");
+  res.send({ express: 'Hello From Express' });
+});
+
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
