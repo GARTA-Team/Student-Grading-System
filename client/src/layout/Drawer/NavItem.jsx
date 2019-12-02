2c8979cc92c91ba339c530ed39fcb87e822da19f
Neacsu Alexandru <aluneacsu@gmail.com>
2019-12-02 22:36:46 +0200

    Housekeeping

--- client/src/layout/Drawer/NavItem.jsx
+++ client/src/layout/Drawer/NavItem.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
