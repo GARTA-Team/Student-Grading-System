8938edd0b391dc3509ecc2771d51f0a425baa53c
Neacsu Alexandru <aluneacsu@gmail.com>
2019-12-04 19:20:16 +0200

    Added axios. Routing now takes into account if user is authenticated.

--- client/src/ui/App.jsx
+++ client/src/ui/App.jsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import axios from 'axios';
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import Home from "./Home";
+import Dashboard from "./Dashboard";
 import Auth from "./Auth";
 import Projects from "./Projects";
 import Team from "./Team";
@@ -32,29 +34,93 @@ const theme = createMuiTheme({
   },
 });
 
-function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Drawer>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/login">
-              <Auth />
-            </Route>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/team">
-              <Team />
-            </Route>
-          </Switch>
-        </Drawer>
-      </Router>
-    </ThemeProvider>
-  );
+export default class App extends Component {
+  state = {
+    isAuthenticated: false,
+    dashboardData: [],
+  }
+
+  constructor(props) {
+    super(props);
+
+    // if for any reason a user becomes loged out redirect him to Home route: "/"
+    axios.interceptors.response.use(
+      response => response,
+      (error) => {
+        if (error.response) {
+          const { status } = error.response;
+
+          switch (status) {
+            case 401:
+              this.setState({ isAuthenticated: false });
+              break;
+
+            default:
+              break;
+          }
+        }
+
+        return Promise.reject(error);
+      },
+    );
+  }
+
+  async componentDidMount() {
+    this.loadDashboardData();
+  }
+
+  loadDashboardData = async () => {
+    try {
+      const data = await axios.get("/dashboard");
+
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+
+  render() {
+    const { isAuthenticated = false } = this.state;
+
+    return (
+      <ThemeProvider theme={theme}>
+        <Router>
+          {
+            isAuthenticated ? (
+              <Drawer>
+                <Switch>
+                  <Route exact path="/">
+                    <Home />
+                  </Route>
+                  <PrivateRoute isAuthenticated={isAuthenticated} path="/dashboard">
+                    <Dashboard />
+                  </PrivateRoute>
+                  <PrivateRoute isAuthenticated={isAuthenticated} path="/login">
+                    <Auth />
+                  </PrivateRoute>
+                  <PrivateRoute isAuthenticated={isAuthenticated} path="/projects">
+                    <Projects />
+                  </PrivateRoute>
+                  <PrivateRoute isAuthenticated={isAuthenticated} path="/team">
+                    <Team />
+                  </PrivateRoute>
+                </Switch>
+              </Drawer>
+            ) : (
+                <Home />
+              )
+          }
+        </Router>
+      </ThemeProvider>
+    );
+  }
 }
 
-export default App;
+
+// eslint-disable-next-line react/prop-types
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  return isAuthenticated
+    ? <Route {...props} />
+    : <Redirect to="/" />;
+};
