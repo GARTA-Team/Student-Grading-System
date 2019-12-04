59516079172353dbe9d443133b965a2e96aad67a
Neacsu Alexandru <aluneacsu@gmail.com>
2019-12-04 13:10:34 +0200

    small changes

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -18,6 +18,13 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"]
+      }
+    }
+  },
   plugins: [
     'react',
   ],
