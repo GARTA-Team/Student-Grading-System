aff1cd792797ef607aa30cfb9b62dc64831cb0c7
Partenie Alex <parteniealex@yahoo.com>
2019-12-05 19:12:31 +0200

    code change

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
   if (req.user) {
     return next();
   }
-  return res.status(401).json({ msg: "no access" });
+  return res.status(403).json({ msg: "no access" });
   // If the user isn't' logged in, redirect them to the login page
   // return res.redirect("/");
 };
