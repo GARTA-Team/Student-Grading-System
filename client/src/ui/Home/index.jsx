96a79c38d6ba2cd61d0f0d296c838ba72e6e19e6
Partenie Alex <parteniealex@yahoo.com>
2019-12-20 17:29:13 +0200

    inter + minor changes

--- client/src/ui/Home/index.jsx
+++ client/src/ui/Home/index.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
+import { makeStyles } from "@material-ui/core/styles";
+import { t } from "react-i18nify";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import LanguagePicker from "../../components/LanguagePicker";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     background: theme.palette.background.default,
-    height:"100vh",
-  
+    height: "100vh",
+
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
+    color: theme.palette.typography.color,
   },
   title: {
     flexGrow: 1,
+    color: theme.palette.typography.color,
   },
   slogan: {
     paddingTop: "5rem",
@@ -35,11 +40,12 @@ export default function Home(props) {
         <AppBar position="static">
           <Toolbar>
             <Typography className={classes.title} variant="h6">
-              Garta Student Grading System
-        </Typography>
-            <Button component={NavLink} to="/login">
-              Login
-          </Button>
+              {t("Home.Title")}
+            </Typography>
+            <LanguagePicker />
+            <Button className={classes.menuButton} component={NavLink} to="/login">
+              {"Login"}
+            </Button>
           </Toolbar>
         </AppBar>
       </Grid>
@@ -47,15 +53,10 @@ export default function Home(props) {
       <Grid item xs={12}>
         <Container maxWidth="sm">
           <Typography className={classes.slogan} variant="h4" color="textPrimary">
-            This is where you're gonna grade your colleagues projects.
-        </Typography>
+            {t("Home.Slogan")}
+          </Typography>
         </Container>
       </Grid>
-
-
     </div>
   );
 }
-
-
-
