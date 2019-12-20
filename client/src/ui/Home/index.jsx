235cfccc39823ea48182f10dfe9dc736cb9cf1f2
Partenie Alex <parteniealex@yahoo.com>
2019-12-20 14:19:48 +0200

    padding needs fixing

--- client/src/ui/Home/index.jsx
+++ client/src/ui/Home/index.jsx
@@ -1,13 +1,61 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 
-export default class index extends Component {
-  render() {
-    return (
-      <div>
-        <p>Home</p>
-        <NavLink to='/login'>Login</NavLink> {/* // TODO */}
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    background: theme.palette.background.default,
+    height:"100vh",
+  
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  slogan: {
+    paddingTop: "5rem",
   }
+}));
+
+export default function Home(props) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid item xs={12}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography className={classes.title} variant="h6">
+              Garta Student Grading System
+        </Typography>
+            <Button component={NavLink} to="/login">
+              Login
+          </Button>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Container maxWidth="sm">
+          <Typography className={classes.slogan} variant="h4" color="textPrimary">
+            This is where you're gonna grade your colleagues projects.
+        </Typography>
+        </Container>
+      </Grid>
+
+
+    </div>
+  );
 }
+
+
+
